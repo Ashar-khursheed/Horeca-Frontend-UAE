15d4c24c3f6af409c6408b6290b42a35df1fc77c
arshadremotedev1 <webdeveloper08@horecastore.ae>
2026-04-02 04:07:21 +0530

    pp

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,6 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
