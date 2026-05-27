da6172d3851f81d5b6eab71ea4ceb952c024be76
arshadremotedev1 <webdeveloper08@horecastore.ae>
2026-05-27 18:36:49 +0530

    api url change

--- src/apis/axios-instance/index.ts
+++ src/apis/axios-instance/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://test-us.thehorecastore.co/api/frontend",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://test-us.thehorecastore.co/api/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
