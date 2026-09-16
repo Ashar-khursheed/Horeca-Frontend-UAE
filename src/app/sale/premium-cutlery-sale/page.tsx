a2fd7c04ba64af4787dfc8e72e1560264ef248a9
devarshadkhan <developer.codewitharsh@gmail.com>
2026-09-16 14:40:21 +0530

    fixed

--- src/app/sale/premium-cutlery-sale/page.tsx
+++ src/app/sale/premium-cutlery-sale/page.tsx
@@ -537,9 +537,9 @@ import { useLocale } from "next-intl";
 import Image from "next/image";
 import { useEffect, useMemo, useRef, useState } from "react";
 // import Imag13 from "../../Asset/opporitnytu/landing page-Picsart-AiImageEnhancer.jpg"
-import Imag13 from "@/assets/banners/cuttely/Untitled design (18) (1).jpg";
+import Imag13 from "@/assets/banners/cuttely/Untitled design (19) (1).jpg";
 // import Imag12 from "../../Asset/opporitnytu/main desktop-Picsart-AiImageEnhancer.jpg";
-import Imag12 from "@/assets/banners/cuttely/Untitled design (18) (1).jpg";
+import Imag12 from "@/assets/banners/cuttely/Untitled design (19) (1).jpg";
 // ─── Page ─────────────────────────────────────────────────────────────────────
 export default function MegaSalePage() {
   const locale = useLocale();
